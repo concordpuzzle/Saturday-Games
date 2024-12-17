ecb2043299779941c85e5f2c146aa9e48807a898
jeremy-marple <jeremy@concordpuzzle.com>
2024-12-17 18:32:41 -0500

    sat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <title>Saturday</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Faculty+Glyphic&family=Orelega+One&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Faculty+Glyphic&family=Orelega+One&family=Ultra&display=swap" rel="stylesheet">
 <script src="https://kit.fontawesome.com/1373d7f0f5.js" crossorigin="anonymous"></script>    <style>
         body {
             display: flex;
@@ -37,6 +37,13 @@
             font-size: 33px;
             padding-top:10px;
         }
+        .logo {
+
+            font-family: "Ultra", serif;
+  font-weight: 400;
+  font-style: normal;
+  color:#b71540;
+        }
         .arvo-bold {
   font-family: "Arvo", serif;
   font-weight: 700;
@@ -50,8 +57,8 @@
 </head>
 <body>
     <div>
-        <div style="text-align: center; margin-bottom: 20px; font-size: 40px; color: #fff;">
-            Saturday Games
+        <div class="logo" style="text-align: center; margin-bottom: 20px; font-size: 40px;">
+            Saturday<br>Games
         
         </div>
         <div class="grid">
