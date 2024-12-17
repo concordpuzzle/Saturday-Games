e08cfaa67ccfc525ab24ed310ef17571a911fc8f
jeremy-marple <jeremy@concordpuzzle.com>
2024-12-17 15:02:23 -0500

    xx

---
+++ resources/views/components/application-mark.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => 'text-[#b71540]']) }} style="font-family: 'Agu Display', serif; font-size: 33px; font-optical-sizing: auto; font-weight: 400; font-style: normal; font-variation-settings: 'MORF' 0;">
+    Saturday
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saturday</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Faculty+Glyphic&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/1373d7f0f5.js" crossorigin="anonymous"></script>    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #0c2461;
+            font-family: "Faculty Glyphic", serif;
+        }
+        .grid {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 10px;
+        }
+        .grid-item {
+            width: 100px;
+            height: 100px;
+            background-color: #fff;
+            border:5px solid #b71540;
+            border-radius: 10px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: white;
+            font-size: 16px;
+        }
+        .white-22{
+            color:#b71540;
+            font-size: 22px;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <div style="text-align: center; margin-bottom: 20px; font-size: 40px; color: #b71540;">
+            Saturday<br>
+        
+        </div>
+        <div class="grid">
+        <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+            <div class="grid-item">
+        <i class="white-22 fa-solid fa-snowflake"></i>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
